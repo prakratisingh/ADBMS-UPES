356cef5cabbef9f3e0e96dbb17c9a3cd7b0dc19a
PRAKRATI SINGH <89121458+prakratisingh@users.noreply.github.com>
2022-07-16 10:53:13 +0530

    Inbuilt functions and relational algebra

---
+++ Experiment 4.sql
@@ -0,0 +1,47 @@
+CREATE TABLE EMP(EMPNO INT PRIMARY KEY, ENAME VARCHAR(20),JOB VARCHAR(30), MGR INT, HIREDATE DATE,SAL INT,COMM INT,DEPTNO INT REFERENCES DEPT(DEPTNO));
+INSERT INTO EMP(EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO)
+              VALUES(7369, 'SMITH',' CLERK', 7902, '1980-12-17', 500, 800, 20),
+              (7499, 'ALLEN', 'SALESMAN', 7698, '1981-02-20', 1600, 300, 30),
+              (7521, 'WARD', 'SALESMAN', 7698, '1981-02-22' ,1250 ,500, 30),
+              (7566, 'JONES', 'MANAGER', 7839, '1981-04-02', 2975 ,NULL ,20),
+              (7654, 'MARTIN', 'SALESMAN', 7698, '1981-09-28', 1250, 1400 ,30),
+              (7698, 'BLAKE', 'MANAGER', 7839, '1981-05-01', 2850,NULL, 30),
+              (7782, 'CLARK', 'MANAGER', 7839, '1981-06-09', 2450,NULL, 10),
+              (7788, 'SCOTT', 'ANALYST',7566, '1982-12-09', 3000,NULL, 20),
+              (7839, 'KING', 'PRESIDENT',NULL, '1981-11-17', 5000,NULL, 10),
+			  (7844, 'TURNER', 'SALESMAN', 7698 ,'1981-09-08', 1500, 0, 30),
+              (7876, 'ADAMS', 'CLERK', 7788, '1983-01-12', 1100,NULL, 20),
+              (7900, 'JAMES', 'CLERK', 7698, '1981-12-03', 950,NULL, 30),
+              (7902, 'FORD', 'ANALYST', 7566, '1981-12-03', 3000,NULL, 20),
+              (7934, 'MILLER', 'CLERK', 7782, '1982-01-23', 1300,NULL, 10);
+CREATE TABLE DEPT(DEPTNO INT PRIMARY KEY , DNAME VARCHAR(10), LOC VARCHAR(20),PRIMARY KEY(DEPTNO));
+INSERT INTO DEPT(DEPTNO,DNAME,LOC) 
+VALUES(10, 'ACCOUNTING', 'NEW YORK'),
+	  (20, 'RESEARCH', 'DALLAS'),
+      (30, 'SALES', 'CHICAGO'),
+      (40, 'OPERATIONS', 'BOSTON');
+/*List the details of the emps whose Salaries more than the employee BLAKE.*/
+SELECT * FROM EMP WHERE SAL>(SELECT SAL FROM EMP WHERE ENAME='BLAKE');
+/*List the emps whose Jobs are same as ALLEN*/
+SELECT * FROM EMP WHERE JOB=(SELECT JOB FROM EMP WHERE ENAME='ALLEN');
+/*List the Emps whose Sal is same as FORD or SMITH in desc order of Names.*/
+SELECT * FROM EMP WHERE SAL IN (SELECT SAL FROM EMP WHERE ( ENAME= 'SMITH' OR ENAME= 'FORD' )) ORDER BY SAL DESC;
+/*List the emps Whose Jobs are same as MILLER or Sal is more than ALLEN.*/
+SELECT * FROM EMP WHERE JOB=(SELECT JOB FROM EMP WHERE ENAME='MILLER') OR SAL>(SELECT SAL FROM EMP WHERE ENAME='ALLEN');
+/*Find the highest paid employee of sales department. */
+SELECT *FROM EMP WHERE SAL=(SELECT MAX(SAL) FROM EMP WHERE JOB='SALESMAN');
+/*List the employees who are senior to most recently hired employee working under king.*/
+SELECT * FROM EMP WHERE HIREDATE<(SELECT MAX(HIREDATE) FROM EMP WHERE MGR IN(SELECT EMPNO FROM EMP WHERE ENAME='KING'));
+/*List the names of the emps who are getting the highest sal dept wise.*/
+SELECT ENAME FROM EMP WHERE SAL=(SELECT MAX(SAL) FROM EMP ORDER BY DEPTNO);
+/*List the emps whose sal is equal to the average of max and minimum*/
+SELECT * FROM EMP WHERE SAL = ((SELECT MAX(SAL) FROM EMP)+(SELECT MIN(SAL) FROM EMP))/2;
+/*List the emps who joined in the company on the same date.*/
+SELECT * FROM EMP E WHERE HIREDATE IN(SELECT HIREDATE FROM EMP WHERE E.EMPNO<>EMPNO);
+/*Find out the emps who joined in the company before their Managers.*/
+SELECT E.ENAME,E.HIREDATE FROM EMP E,EMP M WHERE E.MGR = M.EMPNO AND E.HIREDATE<M.HIREDATE
+
+
+
+
+
